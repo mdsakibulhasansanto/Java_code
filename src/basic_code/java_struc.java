80327367d1fa27ced59295e2afd0d68334ca2e84
md_santo <mdsakibulhasansanto7@gmail.com>
2024-09-22 02:17:32 +0600

    change

--- src/basic_code/java_struc.java
+++ src/basic_code/java_struc.java
@@ -3,6 +3,15 @@ package basic_code;
 public class java_struc {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Md Sakibul hasan santo ");
+        System.out.println(main()+30);
+    }
+
+    static int main(){
+        int a = 10 ;
+        int b = 30;
+        int sum = a+b;
+       return sum;
     }
 }
