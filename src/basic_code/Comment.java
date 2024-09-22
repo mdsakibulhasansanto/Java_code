00974fae59845fefda5cf69b70d53980795d2c8a
md_santo <mdsakibulhasansanto7@gmail.com>
2024-09-22 18:51:58 +0600

    change

---
+++ src/basic_code/Comment.java
@@ -0,0 +1,15 @@
+package basic_code;
+
+public class Comment {
+
+
+    public static void main (String [] args){
+
+        // Single line comment
+
+        /*
+        multi line comment
+        formula
+         */
+    }
+}
--- src/basic_code/java_struc.java
+++ src/basic_code/java_struc.java
@@ -5,13 +5,7 @@ public class java_struc {
     public static void main(String[] args) {
 
         System.out.println("Md Sakibul hasan santo ");
-        System.out.println(main()+30);
     }
 
-    static int main(){
-        int a = 10 ;
-        int b = 30;
-        int sum = a+b;
-       return sum;
-    }
+
 }
