23703bed2709ff8051c8c99c6db5922140a676bb
md_santo <mdsakibulhasansanto7@gmail.com>
2024-09-23 03:49:55 +0600

    change

--- src/operator/relational.java
+++ src/operator/relational.java
@@ -5,9 +5,11 @@ public class relational {
     // { == , != , > , < , >= , <= }
     public static void main(String[] args) {
 
-        int a = 10 ;
-
-        
+        Example example = new Example();
+        example.equal();
+        example.notEqual();
+        example.getterThen();
+        example.lessThen();
 
 
     }
@@ -15,7 +17,67 @@ public class relational {
 
 class Example {
 
+    int a = 10;
 
+    void equal() {
+
+        if (a == 5) {
+            System.out.println("It's true ");
+        } else {
+            System.out.println("It's false ");
+        }
+
+        // Ans false
+    }
+
+    void notEqual() {
+
+        if (a != 6) {
+            System.out.println("It's true ");
+        } else {
+            System.out.println("It's false ");
+        }
+        // Ans true
+    }
+
+    void getterThen() {
+
+        if (a > 6) {
+            System.out.println("It's true ");
+        } else {
+            System.out.println("It's false ");
+        }
+        // Ans true
+    }
+
+    void lessThen() {
+
+        if (a < 6) {
+            System.out.println("It's true ");
+        } else {
+            System.out.println("It's false ");
+        }
+        // Ans true
+    }
+
+    void getterThenOrEqual() {
+        if (a >= 6) {
+            System.out.println("It's true ");
+        } else {
+            System.out.println("It's false ");
+        }
+        // Ans true
+    }
+
+    void lessThenOrEqual() {
+
+        if (a <= 6) {
+            System.out.println("It's true ");
+        } else {
+            System.out.println("It's false ");
+        }
+        // Ans false
+    }
 }
 
 
